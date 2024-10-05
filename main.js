d86d834c799bfc8eee2c76e342a7da39bc3501ea
Anmol Thakur <at2314541@gmail.com>
2024-10-05 21:18:52 +0530

    page2 scroller incomplete

---
+++ main.js
@@ -0,0 +1,32 @@
+let crsr = document.querySelector("#cursor");
+let crbl = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left= dets.x+"px";
+    crsr.style.top= dets.y+"px";
+    crbl.style.left= dets.x-100+"px";
+    crbl.style.top= dets.y-100+"px";
+});
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    duration: 0.5,
+    height: "85px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller: "body",
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1,
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scoller: "body",
+        start: "top -40%",
+        end: "top -70%",
+        scrub: 2
+    }
+})
